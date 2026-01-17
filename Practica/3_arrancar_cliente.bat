2aa6e4e4e9eda0228ec3699f33b46aad71c50d7d
oscarBarEs <106106741+oscarBarEs@users.noreply.github.com>
2026-01-17 20:20:12 +0100

    added jars

--- Practica/3_arrancar_cliente.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-TITLE CLIENTE DE MICROBLOGGING
-echo Iniciando Cliente...
-echo ---------------------------------------
-
-:: Classpath: Necesitamos Comun y Cliente
-java -cp SD_Practica_Comun/bin;SD_Practica_Cliente/bin cliente.Usuario
-
-pause
